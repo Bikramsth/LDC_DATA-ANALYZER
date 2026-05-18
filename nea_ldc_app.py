268daca7f6eb1be09ab1bf8495e697b3a6c86a83
Bikramsth <111862987+Bikramsth@users.noreply.github.com>
2026-05-19 01:45:06 +0545

    Update nea_ldc_app.py

--- nea_ldc_app.py
+++ nea_ldc_app.py
@@ -21,8 +21,6 @@ DATABASE_NAME = "NEA_LDC_Database.db"
 # Custom NEA Color Palette for Visual Clarity
 NEA_PALETTE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
                '#17becf']
-
-
 @st.cache_resource
 def init_db():
     """Initializes the database and auto-cleans old data. Cached so it only runs ONCE per session."""
